b7a542dfa1227ea7838798598533d1f3af668379
HJ_Kim <dr_moms@naver.com>
2021-01-21 11:36:42 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -286,7 +286,7 @@ def encoding_sentence(english_sentence):
         if i == ' ':
             encoded_sentence += ' '
             continue
-        else:
+        elif i.isalpha():
             encoded_sentence += encoding_character(i)
         encoded_sentence += ' '
 
